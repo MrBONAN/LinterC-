541fc52f2b3871dbaec14b583731a762a63b3049
MrBONAN <taleshatrycob@gmail.com>
2024-05-08 16:46:32 +0500

    Пока кривой прототип токенайзера

---
+++ tokenizer_tests.py
@@ -0,0 +1,30 @@
+import unittest
+import tokenizer
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        csharp_code = """
+        using System;
+
+        namespace HelloWorld
+        {
+            class Program
+            {
+                static void Main(string[] args)
+                {
+                    string message = "Hello, world!";
+                    Console.WriteLine(message);
+                }
+            }
+        }
+        """
+
+        parser = tokenizer.Tokenizer(csharp_code)
+        for token in parser.tokenize():
+            #print(f'{token.value} - {token.token_type}, right space: "{token.right_space}"')
+            print((f'{token.value} - {token.token_type}'))
+
+
+if __name__ == '__main__':
+    unittest.main()
