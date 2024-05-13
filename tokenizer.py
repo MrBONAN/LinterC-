150b48f0a201bc9b3e73440adc28ef565b69574b
MrBONAN <taleshatrycob@gmail.com>
2024-05-13 16:25:18 +0500

    Первая рабочая версия токенайзера

--- tokenizer.py
+++ tokenizer.py
@@ -16,88 +16,98 @@ class Token:
 
 class Tokenizer:
     def __init__(self, code):
-        self.code = code
-        self.index = 0
-        self.tokens = []
+        self._code = code
+        self._index = 0
+        self._tokens = []
 
     def get_tokens(self):
-        self.tokens = []
-        while self.index < len(self.code):
-            any([self.try_read_token_word(),
-                self.try_read_string_constant(),
-                self.try_read_operator(),
-                self.read_symbol()])
-        return self.tokens
+        self._tokens = []
+        self._read_spaces()
+        while self._index < len(self._code):
+            for i in [self._try_read_token_word,
+                      self._try_read_string_constant,
+                      self._try_read_operator,
+                      self._read_symbol]:
+                if i():
+                    break
+        return self._tokens
 
-    def try_read_token_word(self):
-        char = self.code[self.index]
+    def _try_read_token_word(self):
+        char = self._code[self._index]
         if not (char.isalnum() or char == '_'):
             return False
-        current_token = self.read_word()
-        spaces = self.read_spaces()
-        if current_token in Tokenizer.KEYWORDS:
-            self.tokens.append(Token(current_token, TokenType.Keyword, spaces))
+        current_token = self._read_word()
+        spaces = self._read_spaces()
+        if current_token in self.KEYWORDS:
+            self._tokens.append(Token(current_token, TokenType.Keyword, spaces))
         else:
-            self.tokens.append(Token(current_token, TokenType.Identifier, spaces))
+            self._tokens.append(Token(current_token, TokenType.Identifier, spaces))
         return True
 
-    def try_read_string_constant(self):
-        char = self.code[self.index]
+    def _try_read_string_constant(self):
+        char = self._code[self._index]
         if char != '"':
             return False
-        current_token = self.read_string_constant()
-        spaces = self.read_spaces()
-        if current_token != '':
-            self.tokens.append(Token(current_token, TokenType.StringConstant, spaces))
+        current_token = self._read_string_constant()
+        spaces = self._read_spaces()
+        self._tokens.append(Token(current_token, TokenType.StringConstant, spaces))
         return True
 
-    def try_read_operator(self):
-        return False
-
-    def read_symbol(self):
-        self.index += 1
-        char = self.code[self.index - 1]
-        self.tokens.append(
-            Token(char, TokenType.Symbol, self.read_spaces()))
+    def _try_read_operator(self):
+        index = self._index
+        if index >= len(self._code):
+            return False
+        op1 = self._code[index]
+        op2 = self._code[index:index + 2] if index + 1 < len(self._code) else None
+        if op2 in self.OPERATIONS:
+            self._index += 2
+            spaces = self._read_spaces()
+            self._tokens.append(Token(op2, TokenType.Operation, spaces))
+        elif op1 in self.OPERATIONS:
+            self._index += 1
+            spaces = self._read_spaces()
+            self._tokens.append(Token(op1, TokenType.Operation, spaces))
+        else:
+            return False
         return True
 
-    def skip_spaces(self):
-        self.read_spaces()
+    def _read_symbol(self):
+        self._index += 1
+        char = self._code[self._index - 1]
+        self._tokens.append(
+            Token(char, TokenType.Symbol, self._read_spaces()))
         return True
 
-    def read_spaces(self):
+    def _read_spaces(self):
         spaces = ''
-        while self.index < len(self.code) and self.code[self.index].isspace():
-            spaces += self.code[self.index]
-            self.index += 1
+        while self._index < len(self._code) and self._code[self._index].isspace():
+            spaces += self._code[self._index]
+            self._index += 1
         return spaces
 
-    def read_word(self):
+    def _read_word(self):
         current_token = ''
-        start = self.index
-        for char in self.code[start:]:
+        start = self._index
+        for char in self._code[start:]:
             if char.isalnum() or char == '_':
                 current_token += char
-                self.index += 1
+                self._index += 1
             else:
                 break
         return current_token
 
-    def read_string_constant(self):
+    def _read_string_constant(self):
         string = ''
-        self.index += 1
-        start = self.index
-        for char in self.code[start:]:
+        self._index += 1
+        start = self._index
+        for char in self._code[start:]:
             if char != '"':
                 string += char
-                self.index += 1
+                self._index += 1
             else:
                 break
-        self.index += 1
+        self._index += 1
         return string
-
-    def try_read_operator(self):
-        pass
 
     KEYWORDS = ['abstract', 'as', 'base', 'bool', 'break', 'byte', 'case',
                 'catch', 'char', 'checked', 'class', 'const', 'continue',
--- tokenizer_tests.py
+++ tokenizer_tests.py
@@ -3,26 +3,82 @@ import tokenizer
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        csharp_code = """
-        using System;
+    def get_tokens(self, csharp_code):
+        parser = tokenizer.Tokenizer(csharp_code)
+        return parser.get_tokens()
 
-        namespace HelloWorld
-        {
-            class Program
-            {
-                static void Main(string[] args)
+    def print_tokens(self, tokens):
+        for token in tokens:
+            print((f'{token.value} - {token.token_type}'))
+
+    def test_string_constant(self):
+        string_constant = '"hello, how are you?"'
+        result = self.get_tokens(string_constant)
+        self.assertEqual(1, len(result))
+        string_token = result[0]
+        self.assertEqual('hello, how are you?', string_token.value)
+        self.assertEqual('', string_token.right_space)
+
+    def tests_large_csharp_code(self):
+        large_csharp_code = """
+                using System;
+
+                namespace HelloWorld
                 {
-                    string message = "Hello, world!";
-                    Console.WriteLine(message);
+                    class Program
+                    {
+                        static void Main(string[] args)
+                        {
+                            string message = "Hello, world!";
+                            Console.WriteLine(message);
+                        }
+                    }
                 }
-            }
+                """
+        result = self.get_tokens(large_csharp_code)
+        self.print_tokens(result)
+
+    OPERATIONS = ['=', '+', '-', '*', '/', '%',
+                  '++', '--', '+=', '-=', '*=', '/=', '%=',
+                  '&&', '^', '||', '==', '!=', '>', '<', '>=', '<=',
+                  '<<', '>>', '&', '|', '^', '~', '?']
+    def test_connected_operations(self):
+        result = self.get_tokens(''.join(self.OPERATIONS))
+        self.assertEqual(len(result), len(self.OPERATIONS))
+        for token, op in zip(result, self.OPERATIONS):
+            self.assertEqual(op, token.value)
+
+    def test_separated_operations(self):
+        result = self.get_tokens(' '.join(self.OPERATIONS))
+        self.assertEqual(len(result), len(self.OPERATIONS))
+        for token, op in zip(result, self.OPERATIONS):
+            self.assertEqual(op, token.value)
+
+    def test_dont_delete_newline_character(self):
+        comment = """some_string
+with_newline_character"""
+        result = self.get_tokens(comment)
+        self.assertEqual(2, len(result))
+        self.assertEqual('\n', result[0].right_space)
+
+    def test_if_statement(self):
+        if_statement = """
+        if (true) {
+            identifier += value * 5;
         }
         """
 
-        parser = tokenizer.Tokenizer(csharp_code)
-        for token in parser.get_tokens():
-            print((f'{token.value} - {token.token_type}'))
+        result = self.get_tokens(if_statement)
+
+
+    def test_while_statement(self):
+        while_statement = """
+        while (5 > 4) {
+            Console.WriteLine("Wow, StringConstant");
+        }
+        """
+
+        self.get_tokens(while_statement)
 
 
 if __name__ == '__main__':
